d23b5eb3eddced821eb6cf6e6300acea52de425d
Jorge <lucky_jjmg@yahoo.com.mx>
2019-11-28 00:14:06 -0600

    Clean

--- Reloj_Micros 2/source/Reloj_Micros 2.c
+++ Reloj_Micros 2/source/Reloj_Micros 2.c
@@ -48,20 +48,8 @@
  */
 int main(void) {
 
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-
-    PRINTF("Hello World\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
-        i++ ;
+
     }
     return 0 ;
 }
